59e098ec07d5b77d2f4983772abc24109fe28c79
Cristian Rojas <cristian.rojas@live.fr>
2024-12-13 17:51:43 +0100

    First commit 🎉

---
+++ TestDrivenAIGenerator/TestDrivenAIGenerator.h
@@ -0,0 +1,18 @@
+//
+//  TestDrivenAIGenerator.h
+//  TestDrivenAIGenerator
+//
+//  Created by Cristian Felipe Patiño Rojas on 13/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestDrivenAIGenerator.
+FOUNDATION_EXPORT double TestDrivenAIGeneratorVersionNumber;
+
+//! Project version string for TestDrivenAIGenerator.
+FOUNDATION_EXPORT const unsigned char TestDrivenAIGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestDrivenAIGenerator/PublicHeader.h>
+
+
---
+++ TestDrivenAIGeneratorTests/TestDrivenAIGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestDrivenAIGeneratorTests.swift
+//  TestDrivenAIGeneratorTests
+//
+//  Created by Cristian Felipe Patiño Rojas on 13/12/24.
+//
+
+import Testing
+@testable import TestDrivenAIGenerator
+
+struct TestDrivenAIGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
