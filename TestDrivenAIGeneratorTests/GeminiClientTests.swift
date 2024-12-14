9af46a91d9461e40bff338162c176978c7d1fbfe
Cristian Rojas <cristian.rojas@live.fr>
2024-12-14 13:26:05 +0100

    Move test logic to a separated HTTPClientSpy

--- TestDrivenAIGeneratorTests/GeminiClientTests.swift
+++ TestDrivenAIGeneratorTests/GeminiClientTests.swift
@@ -10,24 +10,33 @@ import Testing
 
 final class GeminiClient {
     func send() async throws {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")!
+        HTTPClient.shared.data(for: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
     static var shared = HTTPClient()
+    func data(for url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    override func data(for url: URL) {
+        requestedURL = url
+    }
 }
 
 struct GeminiClientTests {
     @Test func does_not_request_data_on_initialization() throws {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let _ = GeminiClient()
         #expect(client.requestedURL == nil)
     }
     
     @Test func requests_data_on_send() async throws {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = GeminiClient()
         _ = try await sut.send()
         #expect(client.requestedURL != nil)
