9b06172862c66751b49fa95012876fbee441760d
Cristian Rojas <cristian.rojas@live.fr>
2024-12-14 13:24:25 +0100

    Request data from URL on send

--- TestDrivenAIGeneratorTests/GeminiClientTests.swift
+++ TestDrivenAIGeneratorTests/GeminiClientTests.swift
@@ -9,17 +9,27 @@ import Foundation
 import Testing
 
 final class GeminiClient {
-    
+    func send() async throws {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")!
+    }
 }
 
 class HTTPClient {
+    static var shared = HTTPClient()
     var requestedURL: URL?
 }
 
 struct GeminiClientTests {
     @Test func does_not_request_data_on_initialization() throws {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         let _ = GeminiClient()
         #expect(client.requestedURL == nil)
+    }
+    
+    @Test func requests_data_on_send() async throws {
+        let client = HTTPClient.shared
+        let sut = GeminiClient()
+        _ = try await sut.send()
+        #expect(client.requestedURL != nil)
     }
 }
