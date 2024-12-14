75875237e28420039a767873da72b8892a7c4a48
Cristian Rojas <cristian.rojas@live.fr>
2024-12-14 12:18:27 +0100

    Add APIKeys holder and gemini api key

---
+++ TestDrivenAIGeneratorTests/APIKeys.swift
@@ -0,0 +1,25 @@
+//
+//  APIKeys.swift
+//  TestDrivenAIGenerator
+//
+//  Created by Cristian Felipe Patiño Rojas on 14/12/24.
+//
+import Foundation
+
+enum APIKeys {
+    static var gemini: String {
+        class BundleFinder {}
+        let bundle = Bundle(for: BundleFinder.self)
+        let path   = bundle.path(forResource: "api_keys", ofType: "plist")!
+        let dict   = NSDictionary(contentsOfFile: path) as! [String: Any]
+        return dict["gemini"] as! String
+    }
+}
+
+import Testing
+
+struct GeminiClientTests {
+    @Test func test() {
+        #expect(APIKeys.gemini.isEmpty == false)
+    }
+}
